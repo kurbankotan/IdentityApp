75079c84b68c3a0f063258759d2088a05e1cd808
Kurban KOTAN <kurbankotan@gmail.com>
2024-08-08 13:13:18 +0300

    Listing Users

--- obj/Debug/net8.0/IdentityApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/IdentityApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eabff18e684c458bb67fd9249897f19f2028f527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18d0e92deab5c436bff257b4d1892cd1b4704fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
